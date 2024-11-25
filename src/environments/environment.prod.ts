4176f446b8e1492d862508f335d7648f154f9ab3
Firas <firasaydi0@gmail.com>
2024-11-25 17:54:17 +0100

    Committing changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDtC8vjyO1qq3eUVlm1caTBvX9tw6AJA4',
+    authDomain: 'touch-meuble.firebaseapp.com',
+    projectId: 'touch-meuble',
+    storageBucket: 'touch-meuble.appspot.com',
+    messagingSenderId: '908767451346',
+    appId: '1:908767451346:web:5d26a73d331804ab99350b',
+  },
 };
